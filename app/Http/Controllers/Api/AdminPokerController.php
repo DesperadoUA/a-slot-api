0731ace1d7b10f02bb85b36a181807daf038f712
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2022-07-26 15:51:33 +0300

    Add AdminPostController

--- app/Http/Controllers/Api/AdminPokerController.php
+++ app/Http/Controllers/Api/AdminPokerController.php
@@ -1,33 +1,9 @@
 <?php
 namespace App\Http\Controllers\Api;
-use Illuminate\Http\Request;
 use App\Services\AdminPokerService;
 
-class AdminPokerController extends BaseController
-{
+class AdminPokerController extends AdminPostController {
     public function __construct() {
         $this->service = new AdminPokerService();
-    }
-    public function index(Request $request) {
-        $settings = [
-            'offset' => $request->has('offset') ? $request->input('offset') : self::OFFSET,
-            'limit' => $request->has('limit') ? $request->input('limit') : self::LIMIT,
-            'order_by' => $request->has('order_by') ? $request->input('order_by') : self::ORDER_BY,
-            'order_key' => $request->has('order_key') ? $request->input('order_key') : self::ORDER_KEY,
-            'lang' => $request->has('lang') ? $request->input('lang') : self::LANG
-        ];
-        return response()->json($this->service->adminIndex($settings));
-    }
-    public function store(Request $request) {
-        return response()->json($this->service->store($request->input('data')));
-    }
-    public function show($id) {
-        return response()->json($this->service->adminShow($id));
-    }
-    public function update(Request $request) {
-        return response()->json($this->service->update($request->input('data')));
-    }
-    public function delete(Request $request) {
-        return response()->json($this->service->delete($request->input('data')));
     }
 }
