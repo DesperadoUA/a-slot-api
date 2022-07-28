8b5f4baeb5eaa1eb92a4d6fcf7b861b2e450df5c
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2022-07-28 11:02:02 +0300

    Refactor PostController

--- app/Http/Controllers/Api/BonusController.php
+++ app/Http/Controllers/Api/BonusController.php
@@ -1,63 +1,10 @@
 <?php
-
 namespace App\Http\Controllers\Api;
-use Illuminate\Http\Request;
+
 use App\Services\BonusService;
 
-class BonusController extends PostController
-{
+class BonusController extends PostController {
     public function __construct() {
         $this->service = new BonusService();
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
-    {
-        $response = [
-            'body' => [],
-            'confirm' => 'error'
-        ];
-        /*
-        $posts = new Posts(['post_type' => self::POST_TYPE]);
-        $settings = [
-            'offset'    => $request->has('offset') ? $request->input('offset') : self::OFFSET,
-            'limit'     => $request->has('limit') ? $request->input('limit') : self::LIMIT,
-            'order_by'  => $request->has('order_by') ? $request->input('order_by') : self::ORDER_BY,
-            'order_key' => $request->has('order_key') ? $request->input('order_key') : self::ORDER_KEY,
-            'lang'      => $request->has('lang') ? $request->input('lang') : self::LANG
-        ];
-        $data = $posts->getPublicPosts($settings);
-        if(!$data->isEmpty()) {
-            $arr = [];
-            foreach ($data as $item) {
-                $arr[] = self::dataCommonDecode($item) + self::dataMetaDecode($item);
-            }
-            $response = [
-                'body' => [
-                    'posts' => $arr,
-                    'total' =>  $posts->getTotalCountPublicByLang(self::POST_TYPE, $settings['lang'])
-                ],
-                'confirm' => 'ok'
-            ];
-        }
-        */
-        return response()->json($response);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id) {
-        return response()->json($this->service->show($id));
-    }
-    public function category($id){
-        return response()->json($this->service->category($id));
     }
 }
--- app/Http/Controllers/Api/PokerController.php
+++ app/Http/Controllers/Api/PokerController.php
@@ -1,70 +1,11 @@
 <?php
-
 namespace App\Http\Controllers\Api;
 
-use App\CardBuilder;
-use App\Models\Cash;
-use App\Models\Category;
-use Illuminate\Http\Request;
-use App\Models\Posts;
-use App\Models\Relative;
 use App\Services\PokerService;
 
 class PokerController extends PostController
 {
     public function __construct() {
         $this->service = new PokerService();
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    const NUMBER_RELATIVE_POSTS = 5;
-    public function index(Request $request)
-    {
-        $response = [
-            'body' => [],
-            'confirm' => 'error'
-        ];
-        /*
-        $posts = new Posts(['post_type' => self::POST_TYPE]);
-        $settings = [
-            'offset'    => $request->has('offset') ? $request->input('offset') : self::OFFSET,
-            'limit'     => $request->has('limit') ? $request->input('limit') : self::LIMIT,
-            'order_by'  => $request->has('order_by') ? $request->input('order_by') : self::ORDER_BY,
-            'order_key' => $request->has('order_key') ? $request->input('order_key') : self::ORDER_KEY,
-            'lang'      => $request->has('lang') ? $request->input('lang') : self::LANG
-        ];
-        $data = $posts->getPublicPosts($settings);
-        if(!$data->isEmpty()) {
-            $arr = [];
-            foreach ($data as $item) {
-                $arr[] = self::dataCommonDecode($item) + self::dataMetaDecode($item);
-            }
-            $response = [
-                'body' => [
-                    'posts' => $arr,
-                    'total' =>  $posts->getTotalCountPublicByLang(self::POST_TYPE, $settings['lang'])
-                ],
-                'confirm' => 'ok'
-            ];
-        }
-        */
-        return response()->json($response);
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id) {
-        return response()->json($this->service->show($id));
-    }
-    public function category($id){
-        return response()->json($this->service->category($id));
     }
 }
